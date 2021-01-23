11894920ed3ad178926d761c4d26e5c48f148eb3
Natalie Perret <natalie.perret.1986@gmail.com>
2021-01-23 10:25:36 +0100

    feat(core): update core nuget version

---
+++ Vp.FSharp.Sql.SqlServer/SqlServerCommand.fs
@@ -0,0 +1,96 @@
+﻿[<RequireQualifiedAccess>]
+module Vp.FSharp.Sql.SqlServer.SqlServerCommand
+
+open System
+open System.Data
+open System.Threading.Tasks
+
+open Microsoft.Data.SqlClient
+
+open Vp.FSharp.Sql
+
+
+/// Initialize a command definition with the given text contained in the given string.
+let text value : SqlServerCommandDefinition =
+    SqlCommand.text value
+
+/// Initialize a command definition with the given text spanning over several strings (ie. list).
+let textFromList value : SqlServerCommandDefinition =
+    SqlCommand.textFromList value
+
+/// Update the command definition so that when executing the command, it doesn't use any logger.
+/// Be it the default one (Global, if any.) or a previously overriden one.
+let noLogger commandDefinition = { commandDefinition with Logger = LoggerKind.Nothing }
+
+/// Update the command definition so that when executing the command, it use the given overriding logger.
+/// instead of the default one, aka the Global logger, if any.
+let overrideLogger value commandDefinition = { commandDefinition with Logger = LoggerKind.Override value }
+
+/// Update the command definition with the given parameters.
+let parameters value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
+    SqlCommand.parameters value commandDefinition
+
+/// Update the command definition with the given cancellation token.
+let cancellationToken value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
+    SqlCommand.cancellationToken value commandDefinition
+
+/// Update the command definition with the given timeout.
+let timeout value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
+    SqlCommand.timeout value commandDefinition
+
+/// Update the command definition and sets the command type (ie. how it should be interpreted).
+let commandType value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
+    SqlCommand.commandType value commandDefinition
+
+/// Update the command definition and sets whether the command should be prepared or not.
+let prepare value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
+    SqlCommand.prepare value commandDefinition
+
+/// Update the command definition and sets whether the command should be wrapped in the given transaction.
+let transaction value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
+    SqlCommand.transaction value commandDefinition
+
+/// Return the sets of rows as an AsyncSeq accordingly to the command definition.
+let queryAsyncSeq connection read (commandDefinition: SqlServerCommandDefinition) =
+    SqlCommand.queryAsyncSeq
+        connection (Constants.Deps) (SqlServerConfiguration.Snapshot) read commandDefinition
+
+/// Return the sets of rows as a list accordingly to the command definition.
+let queryList connection read (commandDefinition: SqlServerCommandDefinition) =
+    SqlCommand.queryList
+        connection (Constants.Deps) (SqlServerConfiguration.Snapshot) read commandDefinition
+
+/// Return the first set of rows as a list accordingly to the command definition.
+let querySetList connection read (commandDefinition: SqlServerCommandDefinition) =
+    SqlCommand.querySetList
+        connection (Constants.Deps) (SqlServerConfiguration.Snapshot) read commandDefinition
+
+/// Return the 2 first sets of rows as a tuple of 2 lists accordingly to the command definition.
+let querySetList2 connection read1 read2 (commandDefinition: SqlServerCommandDefinition) =
+    SqlCommand.querySetList2
+        connection (Constants.Deps) (SqlServerConfiguration.Snapshot) read1 read2 commandDefinition
+
+/// Return the 3 first sets of rows as a tuple of 3 lists accordingly to the command definition.
+let querySetList3 connection read1 read2 read3 (commandDefinition: SqlServerCommandDefinition) =
+    SqlCommand.querySetList3
+        connection (Constants.Deps) (SqlServerConfiguration.Snapshot) read1 read2 read3 commandDefinition
+
+/// Execute the command accordingly to its definition and,
+/// - return the first cell value, if it is available and of the given type.
+/// - throw an exception, otherwise.
+let executeScalar<'Scalar> connection (commandDefinition: SqlServerCommandDefinition) =
+    SqlCommand.executeScalar<'Scalar, _, _, _, _, _, _, _, _, _>
+        connection (Constants.Deps) (SqlServerConfiguration.Snapshot) commandDefinition
+
+/// Execute the command accordingly to its definition and,
+/// - return Some, if the first cell is available and of the given type.
+/// - return None, if first cell is DbNull.
+/// - throw an exception, otherwise.
+let executeScalarOrNone<'Scalar> connection (commandDefinition: SqlServerCommandDefinition) =
+    SqlCommand.executeScalarOrNone<'Scalar, _, _, _, _, _, _, _, _, _>
+        connection (Constants.Deps) (SqlServerConfiguration.Snapshot) commandDefinition
+
+/// Execute the command accordingly to its definition and, return the number of rows affected.
+let executeNonQuery connection (commandDefinition: SqlServerCommandDefinition) =
+    SqlCommand.executeNonQuery
+        connection (Constants.Deps) (SqlServerConfiguration.Snapshot) commandDefinition
--- Vp.FSharp.Sql.SqlServer/Types.fs
+++ Vp.FSharp.Sql.SqlServer/Types.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Data
+open System.Threading.Tasks
 
 open Microsoft.Data.SqlClient
 
@@ -75,17 +76,13 @@ type SqlServerDependencies =
         SqlParameter,
         SqlDataReader,
         SqlTransaction,
+        IsolationLevel,
         SqlServerDbValue>
 
-[<RequireQualifiedAccess>]
-module SqlServerNullDbValue =
-    let ifNone toDbValue = NullDbValue.ifNone toDbValue SqlServerDbValue.Null
-    let ifError toDbValue = NullDbValue.ifError toDbValue (fun _ -> SqlServerDbValue.Null)
+[<AbstractClass; Sealed>]
+type internal Constants private () =
 
-[<RequireQualifiedAccess>]
-module SqlServerCommand =
-
-    let private dbValueToParameter name value =
+    static member DbValueToParameter name value =
         let parameter = SqlParameter()
         parameter.ParameterName <- name
         match value with
@@ -194,92 +191,12 @@ module SqlServerCommand =
             parameter.SqlDbType <- SqlDbType.Variant
         parameter
 
-    let private deps: SqlServerDependencies =
+    static member Deps : SqlServerDependencies =
+        let beginTransactionAsync (connection: SqlConnection) (isolationLevel: IsolationLevel) _ =
+            ValueTask.FromResult(connection.BeginTransaction(isolationLevel))
+
         { CreateCommand = fun connection -> connection.CreateCommand()
+          SetCommandTransaction = fun command transaction -> command.Transaction <- transaction
+          BeginTransactionAsync = beginTransactionAsync
           ExecuteReaderAsync = fun command -> command.ExecuteReaderAsync
-          DbValueToParameter = dbValueToParameter }
-
-    /// Initialize a command definition with the given text contained in the given string.
-    let text value : SqlServerCommandDefinition =
-        SqlCommand.text value
-
-    /// Initialize a command definition with the given text spanning over several strings (ie. list).
-    let textFromList value : SqlServerCommandDefinition =
-        SqlCommand.textFromList value
-
-    /// Update the command definition so that when executing the command, it doesn't use any logger.
-    /// Be it the default one (Global, if any.) or a previously overriden one.
-    let noLogger commandDefinition = { commandDefinition with Logger = LoggerKind.Nothing }
-
-    /// Update the command definition so that when executing the command, it use the given overriding logger.
-    /// instead of the default one, aka the Global logger, if any.
-    let overrideLogger value commandDefinition = { commandDefinition with Logger = LoggerKind.Override value }
-
-    /// Update the command definition with the given parameters.
-    let parameters value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
-        SqlCommand.parameters value commandDefinition
-
-    /// Update the command definition with the given cancellation token.
-    let cancellationToken value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
-        SqlCommand.cancellationToken value commandDefinition
-
-    /// Update the command definition with the given timeout.
-    let timeout value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
-        SqlCommand.timeout value commandDefinition
-
-    /// Update the command definition and sets the command type (ie. how it should be interpreted).
-    let commandType value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
-        SqlCommand.commandType value commandDefinition
-
-    /// Update the command definition and sets whether the command should be prepared or not.
-    let prepare value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
-        SqlCommand.prepare value commandDefinition
-
-    /// Update the command definition and sets whether the command should be wrapped in the given transaction.
-    let transaction value (commandDefinition: SqlServerCommandDefinition) : SqlServerCommandDefinition =
-        SqlCommand.transaction value commandDefinition
-
-    /// Return the sets of rows as an AsyncSeq accordingly to the command definition.
-    let queryAsyncSeq connection read (commandDefinition: SqlServerCommandDefinition) =
-        SqlCommand.queryAsyncSeq
-            connection deps (SqlServerConfiguration.Snapshot) read commandDefinition
-
-    /// Return the sets of rows as a list accordingly to the command definition.
-    let queryList connection read (commandDefinition: SqlServerCommandDefinition) =
-        SqlCommand.queryList
-            connection deps (SqlServerConfiguration.Snapshot) read commandDefinition
-
-    /// Return the first set of rows as a list accordingly to the command definition.
-    let querySetList connection read (commandDefinition: SqlServerCommandDefinition) =
-        SqlCommand.querySetList
-            connection deps (SqlServerConfiguration.Snapshot) read commandDefinition
-
-    /// Return the 2 first sets of rows as a tuple of 2 lists accordingly to the command definition.
-    let querySetList2 connection read1 read2 (commandDefinition: SqlServerCommandDefinition) =
-        SqlCommand.querySetList2
-            connection deps (SqlServerConfiguration.Snapshot) read1 read2 commandDefinition
-
-    /// Return the 3 first sets of rows as a tuple of 3 lists accordingly to the command definition.
-    let querySetList3 connection read1 read2 read3 (commandDefinition: SqlServerCommandDefinition) =
-        SqlCommand.querySetList3
-            connection deps (SqlServerConfiguration.Snapshot) read1 read2 read3 commandDefinition
-
-    /// Execute the command accordingly to its definition and,
-    /// - return the first cell value, if it is available and of the given type.
-    /// - throw an exception, otherwise.
-    let executeScalar<'Scalar> connection (commandDefinition: SqlServerCommandDefinition) =
-        SqlCommand.executeScalar<'Scalar, _, _, _, _, _, _, _, _, _>
-            connection deps (SqlServerConfiguration.Snapshot) commandDefinition
-
-    /// Execute the command accordingly to its definition and,
-    /// - return Some, if the first cell is available and of the given type.
-    /// - return None, if first cell is DbNull.
-    /// - throw an exception, otherwise.
-    let executeScalarOrNone<'Scalar> connection (commandDefinition: SqlServerCommandDefinition) =
-        SqlCommand.executeScalarOrNone<'Scalar, _, _, _, _, _, _, _, _, _>
-            connection deps (SqlServerConfiguration.Snapshot) commandDefinition
-
-    /// Execute the command accordingly to its definition and, return the number of rows affected.
-    let executeNonQuery connection (commandDefinition: SqlServerCommandDefinition) =
-        SqlCommand.executeNonQuery
-            connection deps (SqlServerConfiguration.Snapshot) commandDefinition
+          DbValueToParameter = Constants.DbValueToParameter }
