1baae5bac1d6f1a1856d66ec673f333dd2c8adaf
Mary Perret <mary.perret.1986@gmail.com>
2021-01-23 10:25:36 +0100

    feat(core): update core nuget version

---
+++ Vp.FSharp.Sql.SqlServer/SqlServerNullDbValue.fs
@@ -0,0 +1,8 @@
+﻿[<RequireQualifiedAccess>]
+module Vp.FSharp.Sql.SqlServer.SqlServerNullDbValue
+
+open Vp.FSharp.Sql
+
+
+let ifNone toDbValue = NullDbValue.ifNone toDbValue SqlServerDbValue.Null
+let ifError toDbValue = NullDbValue.ifError toDbValue (fun _ -> SqlServerDbValue.Null)
